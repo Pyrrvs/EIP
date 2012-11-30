75b6a976408630381e52f9dfbc37f304bd4672e4
Christopher <topher.moreau@gmail.com>
2012-11-30 00:29:16 -0800

    Patch ajax

--- public/js/client/user.js
+++ public/js/client/user.js
@@ -1,11 +1,22 @@
 $('document').ready(function() {
 	$('.view-btn').click(function() {
+
+		// <li class="active"><a href="#lA" data-toggle="tab">Section 1</a></li>
+  //       <li class=""><a href="#lB" data-toggle="tab">Section 2</a></li>
+  //       <li class=""><a href="#lC" data-toggle="tab">Section 3</a></li>
+
+			// <div class="tab-pane active" id="lA">
+    		//  <p>I'm in Section A.</p>
+   //          </div>
+
 		$.ajax({
-  			url: $(this).attr('id') + '/resources',
+  			url: $('#username').text() + '/' + $(this).attr('id') + '/resources',
   			type: "GET",
   			success: function(data) {
-    			console.log(data);
-  			}
+  				for (var i = 0; i < data.resources.length; i++) {
+    				console.log(data.resources[i]);
+    			}
+  			},
 		});
 	});
 });
