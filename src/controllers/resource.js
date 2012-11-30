5e017d954dc6939b36188ff98cee496ca8ca32c1
Gauthier Lamaison <gauthier.lamaison@gmail.com>
2012-11-29 23:36:52 -0800

    permissions improvement

--- src/controllers/resource.js
+++ src/controllers/resource.js
@@ -4,8 +4,8 @@ var fs = require('fs');
 var Controller = kNode.Controller.extend({
 
 	ctor : function(app) {
-		this.super(app, null);
-		this.addRoute('/users/:username/:project/addResource', 'POST', this.upload_resource);
+		this.super(app, new (require('../models/resource_mysql.js'))(app));
+		this.addRoute('/users/:username/:project/addResource', 'POST', this.upload_resource, helper.precheck_edit_perm);
 	},
 
 	upload_resource : function(req, res) {
@@ -15,17 +15,27 @@ var Controller = kNode.Controller.extend({
 				helper.internal_server_error(res, err);
 				return ;
 			}
-			fs.rename(req.files.resource_file.path,
-			'./' + user.home + '/' + req.params.project + '/Resources/' + req.body.resource_name,
-			function(err) {
+			var path = './' + user.home + '/' + req.params.project + '/Resources/' + req.body.resource_name;
+			fs.rename(req.files.resource_file.path, path, function(err) {
 				if (err) {
 					helper.internal_server_error(res, err);
 					return ;
-				} else {
-					console.log('insert to database');
 				}
+				project_ctrl.find_by_name_and_owner(req.params.project, req.params.username, function(err, project) {
+					if (err) {
+						helper.internal_server_error(res, err);
+						return ;
+					}
+					// this.model.create({ project_id: project.id, path: path, type: 'image' }, function(err, callback) {
+
+					// });
+				});
 			});
 		});
+	},
+
+	create_resource : function(resource, callback) {
+		this.model.create(resource, callback);
 	}
 });
 
---
+++ src/models/resource_mysql.js
@@ -0,0 +1,30 @@
+var kNode = require('../../util/kNode.js')
+
+module.exports = kNode.Model.extend({
+
+    db : null,
+
+    ctor : function() {
+
+        this.super();
+        this.db = require('./model_mysql.js');
+    },
+
+    create : function(new_resource, callback) {
+        console.log('Creating resource', new_resource);
+        this.db.query().insert('resources',
+                                ['project_id', 'path', 'type'],
+                                [new_project.project_id, new_project.path, new_project.type])
+                        .execute(function(err, result) {
+                            callback(err, result);
+                        });
+    },
+
+    find_by_project_and_type : function(project, type, callback) {
+        this.db.query('SELECT resources.* FROM resources JOIN projects ON resources.project_id = projects.id')
+                    .where('(projects.name=? AND resources.type=?)', [project, type])
+                        .execute(function(err, rows, cols) {
+                            callback(err, rows);
+                        });
+    }
+});
