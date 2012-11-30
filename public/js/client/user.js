8924d5af2ab127ceb6c802aec05246610743e612
Christopher <topher.moreau@gmail.com>
2012-11-30 02:02:21 -0800

    Patch profile view

--- public/js/client/user.js
+++ public/js/client/user.js
@@ -1,22 +1,24 @@
 $('document').ready(function() {
 	$('.view-btn').click(function() {
 
+
 		$.ajax({
-  			url: $('#username').text() + '/' + $(this).attr('id') + '/resources',
-  			type: "GET",
+			type: "GET",
+  			url: window.location.pathname + '/' + $(this).attr('id') + '/resources',
+
   			success: function(data) {
 
+  				$('#file-tab').empty();
+  				$('#file-content').empty();
   				for (var i = 0; i < data.resources.length; i++) {
   					if (i == 0) {
-  						$('#file-tab').append('<li class="active"><a href="#tab-' + i + '" data-toggle="tab">' + data.resources[i].name + '</a></li>');
+  						$('#file-tab').append('<li class="file-tab active"><a href="#tab-' + i + '" data-toggle="tab">' + data.resources[i].name + '</a></li>');
   						$('#file-content').append('<div class="tab-pane active" id="tab-' + i + '">' + data.resources[i].type + '</div>');
   					}
   					else {
   						$('#file-tab').append('<li class=""><a href="#tab-' + i + '" data-toggle="tab">' + data.resources[i].name + '</a></li>');
   						$('#file-content').append('<div class="tab-pane" id="tab-' + i + '">' + data.resources[i].type + '</div>');
   					}
-
-    				console.log(data.resources[i]);
     			}
   			},
 		});
