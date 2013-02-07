7b72e528911de103fc852b313077c301d38441f8
Kraoz <main.kraoz@gmail.com>
2013-02-06 18:42:25 -0800

    42

---
+++ Rails/AGB/app/assets/javascripts/util/backbone-model++.js
@@ -0,0 +1,35 @@
+Backbone.Model.prototype.rebind = function(a, b, c, d) {
+
+  try {
+    this.unbind(a, b, c);
+  } catch (e) {}
+  this.bind(a, b, c);
+  if (d)
+    b.call(c, this, true);
+};
+
+Backbone.Collection.prototype.rebind = function(a, b, c, d) {
+
+  this.unbind(a, b, c);
+  this.bind(a, b, c);
+  if (d && a == "add") {
+    var self = this;
+    this.each(function(model) {
+      b.call(c, model, self, true)
+    });
+  }
+};
+
+Backbone.Model.prototype.deepClone = function(cloneUserMembers) {
+
+  var clone = this.clone();
+  clone.attributes = $.extend(true, {}, this.attributes);
+  if (cloneUserMembers)
+    for (var i in this)
+      if (!_.has(clone, i))
+        clone[i] = this[i];
+  for (var i in clone.attributes)
+    if (clone.attributes[i] instanceof Backbone.Model)
+      clone.attributes[i] = clone.attributes[i].deepClone(cloneUserMembers);
+  return (clone);
+};
---
+++ Rails/AGB/app/assets/javascripts/util/jquery.utils.js
@@ -0,0 +1,34 @@
+$(function() {
+
+	$("body").on("keydown", 'input[type="text"]', function(e) {
+
+		if (e.keyCode == 13)
+			$(this).trigger("validate");
+	});
+
+	$("body").on("click", ".accordion-heading a", function(e) {
+
+		var open = !$(e.target).parent().parent().find(".accordion-body").hasClass("in");
+
+		$(this).parent().trigger(open ? "open" : "close", this);
+	});
+
+	$("body").on("click", ".nav-tabs li", function(e) {
+
+		if ($(this).hasClass("disabled"))
+			return (false);
+	});
+
+	$("body").on("click", ".tab", function(e) {
+
+		var nameClass = "." + $(e.target).attr("name");
+
+		$(nameClass).hide().parent().find(nameClass + '[data-type="' + $(e.target).data("type") + '"]').show().trigger("change");
+	});
+
+	$("body").on("change", 'input[type="number"]', function(e) {
+
+		if (_.isNaN(parseInt($(e.target).val())))
+			$(e.target).val(0);
+	});
+});
